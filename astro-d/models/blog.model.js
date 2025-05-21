263b5f421f2c70e8a785bfcfc868233e718e3271
Dilan <dilanderegizu@icloud.com>
2025-05-21 17:24:10 +0300

    Update blog.model.js

--- astro-d/models/blog.model.js
+++ astro-d/models/blog.model.js
@@ -5,16 +5,22 @@ const blogSchema = new Schema(
   {
     title: {
       type: String,
+      required: true,
     },
     content: {
       type: String,
+      required: true,
     },
     date: {
       type: Date,
       default: Date.now,
     },
   },
-  { minimize: true, autoIndex: true, timestamps: true }
+  {
+    minimize: true,
+    autoIndex: true,
+    timestamps: true,
+  }
 );
 
 const Blog = mongoose.model("Blog", blogSchema, "blogs");
